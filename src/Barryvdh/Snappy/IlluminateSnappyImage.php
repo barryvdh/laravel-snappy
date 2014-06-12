518d02507fe1cbbe481edb37f7fb80b3f0811f8b
Bradley Weston <brad@legalweb.org.uk>
2014-06-12 11:51:17 +0100

    Use Illuminate Filesystem.
    Make snappy use the Illuminate file system. This is actually almost a
    must.

---
+++ src/Barryvdh/Snappy/IlluminateSnappyImage.php
@@ -0,0 +1,103 @@
+<?php namespace 
+
+use Knp\Snappy\Image;
+
+class IlluminateSnappyImage extends Image {
+
+	/**
+	 * @param \Illuminate\Filesystem\Filesystem
+     * @param string $binary
+     * @param array $options
+	 */
+	public function __construct(Filesystem $fs, $binary, array $options)
+	{
+		parent::__construct($binary, $options);
+
+		$this->fs = $fs;
+	}
+
+    /**
+     * Wrapper for the "file_get_contents" function
+     *
+     * @param string $filename
+     *
+     * @return string
+     */
+    protected function getFileContents($filename)
+    {
+        return $this->fs->get($filename);
+    }
+
+    /**
+     * Wrapper for the "file_exists" function
+     *
+     * @param string $filename
+     *
+     * @return boolean
+     */
+    protected function fileExists($filename)
+    {
+        return $this->fs->exists($filename);
+    }
+
+    /**
+     * Wrapper for the "is_file" method
+     *
+     * @param string $filename
+     *
+     * @return boolean
+     */
+    protected function isFile($filename)
+    {
+        return $this->fs->isFile($filename);
+    }
+
+    /**
+     * Wrapper for the "filesize" function
+     *
+     * @param string $filename
+     *
+     * @return integer or FALSE on failure
+     */
+    protected function filesize($filename)
+    {
+        return $this->fs->size($filename);
+    }
+
+    /**
+     * Wrapper for the "unlink" function
+     *
+     * @param string $filename
+     *
+     * @return boolean
+     */
+    protected function unlink($filename)
+    {
+        return $this->fs->delete($filename);
+    }
+
+    /**
+     * Wrapper for the "is_dir" function
+     *
+     * @param string $filename
+     *
+     * @return boolean
+     */
+    protected function isDir($filename)
+    {
+        return $this->fs->isDirectory($filename);
+    }
+
+    /**
+     * Wrapper for the mkdir function
+     *
+     * @param string $pathname
+     *
+     * @return boolean
+     */
+    protected function mkdir($pathname)
+    {
+        return $this->fs->makeDirectory($pathname, 0777, true, true);
+    }
+
+}
