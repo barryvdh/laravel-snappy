91c0597d6d981a821079f110ea771bbedf2b8493
sattip <sattip@hotmail.com>
2015-09-21 23:55:51 +0300

    add $env array as input

--- config/snappy.php
+++ config/snappy.php
@@ -5,15 +5,17 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => '/usr/local/bin/wkhtmltopdf',
+        'binary'  => '/usr/local/bin/wkhtmltopdf',
         'timeout' => false,
         'options' => array(),
+        'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary' => '/usr/local/bin/wkhtmltoimage',
+        'binary'  => '/usr/local/bin/wkhtmltoimage',
         'timeout' => false,
         'options' => array(),
+        'env'     => array(),
     ),
 
 
