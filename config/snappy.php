02345b4d362c8a50e42953ccadd626dd3dbff24a
Steve Bauman <steven_bauman@outlook.com>
2019-03-14 02:37:29 -0400

    Configuration tweaks with documentation (#338)
    * Configuration tweaks with documentation
    
    - Use PHP 5.4 array syntax for consistency with Laravel configuration files
    - Added configuration documentation with references (should clear up some confusion on issues)
    
    * Added missing Env doc

--- config/snappy.php
+++ config/snappy.php
@@ -1,22 +1,52 @@
 <?php
 
-return array(
+return [
 
-
-    'pdf' => array(
+    /*
+    |--------------------------------------------------------------------------
+    | Snappy PDF / Image Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This option contains settings for PDF generation.
+    |
+    | Enabled:
+    |    
+    |    Whether to load PDF / Image generation.
+    |
+    | Binary:
+    |    
+    |    The file path of the wkhtmltopdf / wkhtmltoimage executable.
+    |
+    | Timout:
+    |    
+    |    The amount of time to wait (in seconds) before PDF / Image generation is stopped.
+    |    Setting this to false disables the timeout (unlimited processing time).
+    |
+    | Options:
+    |
+    |    The wkhtmltopdf command options. These are passed directly to wkhtmltopdf.
+    |    See https://wkhtmltopdf.org/usage/wkhtmltopdf.txt for all options.
+    |
+    | Env:
+    |
+    |    The environment variables to set while running the wkhtmltopdf process.
+    |
+    */
+    
+    'pdf' => [
         'enabled' => true,
         'binary'  => '/usr/local/bin/wkhtmltopdf',
         'timeout' => false,
-        'options' => array(),
-        'env'     => array(),
-    ),
-    'image' => array(
+        'options' => [],
+        'env'     => [],
+    ],
+    
+    'image' => [
         'enabled' => true,
         'binary'  => '/usr/local/bin/wkhtmltoimage',
         'timeout' => false,
-        'options' => array(),
-        'env'     => array(),
-    ),
+        'options' => [],
+        'env'     => [],
+    ],
 
-
-);
+];
