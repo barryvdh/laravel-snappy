219461169573d1df1709cc12504b650143d027be
Barry vd. Heuvel <barry@fruitcake.nl>
2022-01-29 09:51:19 +0100

    Add initial tests

---
+++ tests/PdfTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Barryvdh\Snappy\Tests;
+
+use Barryvdh\Snappy\Facade;
+use Barryvdh\Snappy\Facades\SnappyPdf;
+use Illuminate\Http\Response;
+
+class PdfTest extends TestCase
+{
+    public function testAlias(): void
+    {
+        $pdf = \PDF::loadHtml('<h1>Test</h1>');
+        /** @var Response $response */
+        $response = $pdf->download('test.pdf');
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertNotEmpty($response->getContent());
+        $this->assertEquals('application/pdf', $response->headers->get('Content-Type'));
+        $this->assertEquals('attachment; filename="test.pdf"', $response->headers->get('Content-Disposition'));
+    }
+
+    public function testFacade(): void
+    {
+        $pdf = SnappyPdf::loadHtml('<h1>Test</h1>');
+        /** @var Response $response */
+        $response = $pdf->download('test.pdf');
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertNotEmpty($response->getContent());
+        $this->assertEquals('application/pdf', $response->headers->get('Content-Type'));
+        $this->assertEquals('attachment; filename="test.pdf"', $response->headers->get('Content-Disposition'));
+    }
+
+    public function testDownload(): void
+    {
+        $pdf = SnappyPdf::loadHtml('<h1>Test</h1>');
+        /** @var Response $response */
+        $response = $pdf->download('test.pdf');
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertNotEmpty($response->getContent());
+        $this->assertEquals('application/pdf', $response->headers->get('Content-Type'));
+        $this->assertEquals('attachment; filename="test.pdf"', $response->headers->get('Content-Disposition'));
+    }
+
+    public function testStream(): void
+    {
+        $pdf = SnappyPdf::loadHtml('<h1>Test</h1>');
+        /** @var Response $response */
+        $response = $pdf->stream('test.pdf');
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertNotEmpty($response->getContent());
+        $this->assertEquals('application/pdf', $response->headers->get('Content-Type'));
+        $this->assertEquals('inline; filename="test.pdf"', $response->headers->get('Content-Disposition'));
+    }
+
+    public function testView(): void
+    {
+        $pdf = SnappyPdf::loadView('test');
+        /** @var Response $response */
+        $response = $pdf->download('test.pdf');
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertNotEmpty($response->getContent());
+        $this->assertEquals('application/pdf', $response->headers->get('Content-Type'));
+        $this->assertEquals('attachment; filename="test.pdf"', $response->headers->get('Content-Disposition'));
+    }
+
+}
